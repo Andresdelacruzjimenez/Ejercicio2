c2f2f73eb81f3201b2a22f0e2c2915557ac3c50a
Andrés <andres.saludambiental@gmail.com>
2021-02-01 19:17:44 +0100

    Todos los test a realizar

---
+++ Ejercicio2/src/SalarioTest.java
@@ -0,0 +1,62 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SalarioTest {
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testSalario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSalarioStringFloatFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetVentasMes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetVentasMes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetHorasExtra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetHorasExtra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalculaSalarioBruto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalculaSalarioNeto() {
+		fail("Not yet implemented");
+	}
+
+}
